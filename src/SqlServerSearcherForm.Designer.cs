2a0c07de26b1acfb38f267d7e855d0efa231db2b
Allan Simonsen <allansimonsen1972@gmail.com>
2017-01-26 05:06:28 +0100

    Initial commit.

---
+++ src/SqlServerSearcherForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace SQLServerSearcher
+{
+    partial class SqlServerSearcherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Tables");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Views");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Stored procedures");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Functions");
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbFindText = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkTables = new System.Windows.Forms.CheckBox();
+            this.chkStoredProcedures = new System.Windows.Forms.CheckBox();
+            this.chkViews = new System.Windows.Forms.CheckBox();
+            this.chkFunctions = new System.Windows.Forms.CheckBox();
+            this.chkMatchCase = new System.Windows.Forms.CheckBox();
+            this.tvResults = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbServer = new System.Windows.Forms.ComboBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFind.Location = new System.Drawing.Point(756, 101);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(138, 35);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Find what:";
+            // 
+            // cmbFindText
+            // 
+            this.cmbFindText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbFindText.FormattingEnabled = true;
+            this.cmbFindText.Location = new System.Drawing.Point(12, 82);
+            this.cmbFindText.Name = "cmbFindText";
+            this.cmbFindText.Size = new System.Drawing.Size(557, 28);
+            this.cmbFindText.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(583, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Look in:";
+            // 
+            // chkTables
+            // 
+            this.chkTables.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkTables.AutoSize = true;
+            this.chkTables.Location = new System.Drawing.Point(587, 31);
+            this.chkTables.Name = "chkTables";
+            this.chkTables.Size = new System.Drawing.Size(82, 24);
+            this.chkTables.TabIndex = 4;
+            this.chkTables.Text = "Tables";
+            this.chkTables.UseVisualStyleBackColor = true;
+            // 
+            // chkStoredProcedures
+            // 
+            this.chkStoredProcedures.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkStoredProcedures.AutoSize = true;
+            this.chkStoredProcedures.Location = new System.Drawing.Point(690, 35);
+            this.chkStoredProcedures.Name = "chkStoredProcedures";
+            this.chkStoredProcedures.Size = new System.Drawing.Size(167, 24);
+            this.chkStoredProcedures.TabIndex = 6;
+            this.chkStoredProcedures.Text = "Stored procedures";
+            this.chkStoredProcedures.UseVisualStyleBackColor = true;
+            // 
+            // chkViews
+            // 
+            this.chkViews.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkViews.AutoSize = true;
+            this.chkViews.Location = new System.Drawing.Point(587, 65);
+            this.chkViews.Name = "chkViews";
+            this.chkViews.Size = new System.Drawing.Size(77, 24);
+            this.chkViews.TabIndex = 7;
+            this.chkViews.Text = "Views";
+            this.chkViews.UseVisualStyleBackColor = true;
+            // 
+            // chkFunctions
+            // 
+            this.chkFunctions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkFunctions.AutoSize = true;
+            this.chkFunctions.Location = new System.Drawing.Point(690, 65);
+            this.chkFunctions.Name = "chkFunctions";
+            this.chkFunctions.Size = new System.Drawing.Size(105, 24);
+            this.chkFunctions.TabIndex = 8;
+            this.chkFunctions.Text = "Functions";
+            this.chkFunctions.UseVisualStyleBackColor = true;
+            // 
+            // chkMatchCase
+            // 
+            this.chkMatchCase.AutoSize = true;
+            this.chkMatchCase.Location = new System.Drawing.Point(12, 116);
+            this.chkMatchCase.Name = "chkMatchCase";
+            this.chkMatchCase.Size = new System.Drawing.Size(117, 24);
+            this.chkMatchCase.TabIndex = 10;
+            this.chkMatchCase.Text = "Match case";
+            this.chkMatchCase.UseVisualStyleBackColor = true;
+            // 
+            // tvResults
+            // 
+            this.tvResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvResults.Location = new System.Drawing.Point(0, 0);
+            this.tvResults.Name = "tvResults";
+            treeNode1.Name = "NodeTables";
+            treeNode1.Text = "Tables";
+            treeNode2.Name = "ViewsNode";
+            treeNode2.Text = "Views";
+            treeNode3.Name = "StoredProceduresNode";
+            treeNode3.Text = "Stored procedures";
+            treeNode4.Name = "FunctionsNode";
+            treeNode4.Text = "Functions";
+            this.tvResults.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            this.tvResults.Size = new System.Drawing.Size(292, 550);
+            this.tvResults.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Server:";
+            // 
+            // cmbServer
+            // 
+            this.cmbServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbServer.FormattingEnabled = true;
+            this.cmbServer.Location = new System.Drawing.Point(12, 27);
+            this.cmbServer.Name = "cmbServer";
+            this.cmbServer.Size = new System.Drawing.Size(557, 28);
+            this.cmbServer.TabIndex = 13;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(16, 148);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvResults);
+            this.splitContainer1.Size = new System.Drawing.Size(878, 550);
+            this.splitContainer1.SplitterDistance = 292;
+            this.splitContainer1.TabIndex = 14;
+            // 
+            // SqlServerSearcherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 710);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.cmbServer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkMatchCase);
+            this.Controls.Add(this.chkFunctions);
+            this.Controls.Add(this.chkViews);
+            this.Controls.Add(this.chkStoredProcedures);
+            this.Controls.Add(this.chkTables);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbFindText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFind);
+            this.Name = "SqlServerSearcherForm";
+            this.Text = "Sql Server Searcher";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFindText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkTables;
+        private System.Windows.Forms.CheckBox chkStoredProcedures;
+        private System.Windows.Forms.CheckBox chkViews;
+        private System.Windows.Forms.CheckBox chkFunctions;
+        private System.Windows.Forms.CheckBox chkMatchCase;
+        private System.Windows.Forms.TreeView tvResults;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbServer;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
